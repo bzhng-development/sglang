457ec2b0f7324256dbc0ec5a506ae14a5ae56db1
fzyzcjy <ch271828n@outlook.com>
2026-02-26 22:46:24 +0800

    Revert "fix e2e test: add dims='t h' to dumper.dump() for comparator unshard"
    This reverts commit 249c17209ebf12103941b8a01f44dbb34c1b9af8.

--- test/registered/debug_utils/test_engine_dumper_comparator_e2e.py
+++ test/registered/debug_utils/test_engine_dumper_comparator_e2e.py
@@ -46,9 +46,9 @@ patches:
               hidden_states=hidden_states,
               forward_batch=forward_batch,
           )
-        append: "dumper.dump('patched_attn_output', hidden_states, dims='t h')"
+        append: "dumper.dump('patched_attn_output', hidden_states)"
       - match: "hidden_states = self.mlp(hidden_states)"
-        append: "dumper.dump('patched_mlp_output', hidden_states, dims='t h')"
+        append: "dumper.dump('patched_mlp_output', hidden_states)"
 """
 
 
